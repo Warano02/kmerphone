9f0fbef34c763f2af0f0cdc61c6b91e1b0e27a83
Warano02 <carineteoi@gmail.com>
2025-09-28 01:29:44 +0100

    phone viewers components

--- src/components/common/Phone.tsx
+++ src/components/common/Phone.tsx
@@ -1,23 +1,26 @@
 import type { ProductCollectionProps } from "@/types"
 import { Link } from "react-router"
-function Phone({phone}: ProductCollectionProps) {
+function Phone({ phone }: ProductCollectionProps) {
     return (
-        <Link to={`/products/${phone.id}`} className="relative rounded  w-[312px] h-[512px] flex flex-col overflow-hidden  bg-white ">
-            <div className="group w-full h-[312px]   cursor-pointer">
+        <Link to={`/products/${phone.id}`} className="relative rounded  w-[249px] h-[357px] flex flex-col overflow-hidden  bg-white ">
+            <div className="group w-full h-[289px]   cursor-pointer">
                 <div className="relative w-full h-full transition-transform duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
                     <div className="absolute w-full h-full [backface-visibility:hidden] flex items-center justify-center ">
-                        <img src={phone.images[0]}  alt="" />
+                        <img src={phone.images[0]} className="w-full h-full object-cover" alt="" />
                     </div>
 
                     <div className="absolute w-full h-full [backface-visibility:hidden] flex items-center justify-center   text-white [transform:rotateY(180deg)]">
-                        <img src={phone.images[1]} alt="" />
+                        <img src={phone.images[1]} className="w-full h-full object-cover" alt="" />
                     </div>
                 </div>
             </div>
-            <p className="w-full text-center font-monserat text-secondary px-2 font-semibold">
-                {phone.desc}
+            <p className="w-full text-[16px] font-monserat text-[#030303] px-2 font-semibold">
+                {phone.desc.slice(0, 62).concat("...")}
             </p>
-
+            <div className="w-full mt-3 gap-1">
+                <span className="text-[19px] text-[#CA0000] font-bold font-monserat ">{phone.price} FCFA </span>
+                <span className="text-[14px] text-[#757575] font-bold font-monserat line-through ">{phone.normalPrice} FCFA </span>
+            </div>
         </Link>
     )
 }
